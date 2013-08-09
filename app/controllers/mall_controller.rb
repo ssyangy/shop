6b4aadba2d2fa3bdf51ff6051ad60f5d9fc31062
Fermin <sufeng00@gmail.com>
2013-08-09 17:25:10 +0800

    fix

--- app/controllers/mall_controller.rb
+++ app/controllers/mall_controller.rb
@@ -5,14 +5,14 @@ class MallController < ApplicationController
   		  @p_category = MCategory.find(params[:p1])
   		  @ch_category = MCategory.find params[:p2]
   		  @ch_categories = @p_category.children
-		  @products = Product.where(:m_category_id => @ch_category.subtree_ids)
+		  @products = Product.selling.where(:m_category_id => @ch_category.subtree_ids)
 		elsif params[:p1]
 		  @p_category = MCategory.find(params[:p1])
 		  @ch_categories = @p_category.children
-  		  @products = Product.where(:m_category_id => @p_category.subtree_ids)
+  		  @products = Product.selling.where(:m_category_id => @p_category.subtree_ids)
 		else
 		  @ch_categories = MCategory.where(:ancestry_depth => 2)
-		  @products = Product.where(:m_category_id => MCategory.select('id').map(&:id))
+		  @products = Product.selling.where(:m_category_id => MCategory.select('id').map(&:id))
 		end
 	end
 end
